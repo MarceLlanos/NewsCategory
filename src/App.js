cde60c9c07f3567f88b0c16c861236b620ef2061
Marcela Llanos <marcela3llanos@gmail.com>
2019-09-15 22:30:31 -0400

    Se realizo la conexion con la api de newsapi

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 
 class App extends Component{
+
+  state = {
+    noticias:{}
+  }
+
+  componentDidMount(){
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = () => {
+    let url = "https://newsapi.org/v2/top-headlines?country=mx&category=business&apikey=";
+    fetch(url).then(respuesta => {
+      respuesta.json();
+    }).then(noticias => {
+      this.setState({noticias : noticias.articles});
+    })
+  }
   render(){
     return (
       <div className="App">

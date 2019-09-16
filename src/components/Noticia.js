095f057afb0ff121a564a329d45946aeea4c9480
Marcela Llanos <marcela3llanos@gmail.com>
2019-09-15 23:23:25 -0400

    Se realizo el formulario de noticia con los datos sacados de la api

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -1,8 +1,23 @@
-import React from 'react'
-const Noticia = () => {
+import React from 'react';
+
+const Noticia = (noticia) => {
+	
+	const {author, urlToImage, title, description, source} = noticia.noticia;
   return ( 
-    <div>
-      <p>Desde noticia</p>
+    <div className = "col s12 m6 l4 ">
+      <div className = "card">
+				<div className = "card-image">
+					<img src = {urlToImage} alt = {title}/>
+					<span className = "card-title">{source.name}</span>
+				</div>
+				<div className = "card-content">
+					<h3>{title}</h3>
+					<p>{description}</p>
+				</div>
+				<div className = "card-action">
+					<a href = {url} target = "_blank" className = "waves-effects waves-light btn">Noticia Completa</a>
+				</div>
+			</div>
     </div>
    );
 }
